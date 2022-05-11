02372d03fe4725c2562edcef84c53dd146c4f0af
Flora Arnoud <floracampos136@gmail.com>
2022-05-11 18:59:42 -0300

    Alterei index.js

---
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,66 @@
+
+//Interpretação
+//1
+//let a = 10
+//let b = 10
+//console.log(b)
+//10
+//b = 5
+//console.log(a,b)
+// 10 5
+
+//2
+//let a = 10
+//let b = 20
+//c = a
+//b = c
+//a = b
+//console.log(a,b,c)
+// 10 10 10
+
+//3
+// p = horasTrab
+// t = quantRec
+
+//Escritas
+//1
+//let nome = null 
+//let idade
+//console.log(nome,idade)
+//typeof nome 
+//typeof idade
+//Apareceu undefined, pois não foi declarado nenhum valor
+//nas variáveis
+//nome = prompt ("Qual é o seu nome")
+//idade = prompt ("Qual sua idade?")
+//console.log(nome,idade)
+
+//2
+//let perguntaUm = "Sim?"
+//let perguntaDois = "Não?"
+//let perguntaTres = "Talvez?"
+//let respostaUm = prompt("Sim?")
+//let respostaDois = prompt("Não?") 
+//let respostaTres = prompt("Talvez?")
+//console.log(perguntaUm,respostaUm)
+//console.log(perguntaDois,respostaDois)
+//console.log(perguntaTres,respostaTres)
+
+//3
+//let a = 10
+//let b = 25
+//var aux = a
+//a = b
+//b = aux 
+//console.log("O novo valor de a é", a)
+//console.log("O novo valor de b é", b)
+
+
+
+
+
+
+
+
+
+
