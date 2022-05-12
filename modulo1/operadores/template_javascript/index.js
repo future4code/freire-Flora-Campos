11e239662c87cc73ade9d9b3a14988d9fa4fb372
Flora Arnoud <floracampos136@gmail.com>
2022-05-12 07:21:18 -0300

    Alterei index.js

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,51 @@
+//Interpretacao
+//1
+//const bool1 = true
+//const bool2 = false
+//const bool3 = !bool2
+
+//let resultado = bool1 && bool2
+//console.log("a.", resultado)
+// false
+//resultado = bool1 && bool2 && bool3
+//console.log("b.", resultado)
+//true
+//resultado = !resultado && (bool1 || bool2)
+//console.log("c.", resultado) 
+//true
+//console.log("d.", typeof resultado)
+//bolean
+
+//2
+//let primeiroNumero = prompt("Digite um número!")
+//let segundoNumero = prompt("Digite outro número!")
+//const soma = primeiroNumero + segundoNumero
+//console.log(soma)
+
+//Escrita
+//1
+//let idade = prompt("Quantos anos você tem?")
+//let idade2 = prompt("E qual a idade da sua amiga?")
+//let resposta = idade > idade2
+//console.log("Sua idade é maior do que a da sua amiga?", resposta)
+
+//2
+//let numeroPar = prompt("Insira número par:")
+//const restoDaDiv = numeroPar % 2
+//console.log(restoDaDiv)
+
+//3
+//let idade = prompt("Quantos anos você tem?")
+//const meses = idade * 12
+//const dias = idade * 30
+//const horas = idade * 24
+//console.log(meses,dias,horas)
+
+
+
+
+
+
+
+
+
