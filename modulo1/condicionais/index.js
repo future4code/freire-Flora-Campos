9ec890d15b7a7df595c2d2a1a7c0e3f09dab8df4
Flora Arnoud <floracampos136@gmail.com>
2022-05-17 18:10:46 -0300

    Atividades Condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,70 @@
+//Interpretação
+//1
+//a)A partir do número que o usário for digitar
+//o código ira comparar se a sua difernça com 2 é idêntico a zero.
+
+//b)Números pares, dividendos de 2.
+
+//Números ímpares.
+
+//2
+//A) Ele lê a fruta que foi escolhida pelo usário e imprime o preço dela.
+//B) 2,25
+//c)Ia dar erro de leitura, pois o break separa um caso do outro.
+
+//3
+//A) Criando uma variavel que guarda o valor do prompt
+//B) Se for dez será "Esse número passou o teste", se for -10 não aparecerá nada.
+//C) o console.log(mensagem) não vai conseguir imprimir o valor pois está em um escopo fechado.
+
+////Escrita
+//1
+//const idade= Number(prompt("Digite sua idade:"))
+//if (idade >= 18){
+   // console.log("Você pode dirigir!")
+//}else {
+   // console.log("Você não pode dirigir")
+//}
+//2
+//const turno= prompt("Digite seu turno. V, M ou N?")
+//let Vespertino = "V"
+//let Matutino = "M"
+//let Noturno = "N" 
+//if (turno === Vespertino){
+  //  console.log("Boa tarde!")
+//} else if( turno === Matutino){
+  //  console.log("Bom dia!")
+//} else if( turno === Noturno){
+  //  console.log("Boa noite!")
+//}
+
+//3
+//let saudacao
+//switch(turno){
+   // case "M":
+       // saudacao = "Bom dia"
+  //  break;
+//case "N":
+   // saudacao = "Boa noite"
+    //break;
+//case "V":
+   // saudacao = "Boa tarde"
+   // break;
+//}
+//console.log (saudacao)
+
+//4
+//const genero = prompt("Qual o gênero?")
+//const preco = prompt("Qual o preço?")
+
+
+//if(genero === "fantasia" && preco <= 15) {
+  //  console.log("Bom filme")
+//} else{
+ //   console.log("Escolha outro filme")
+//}
+
+//Desafio
+//const lanchinho= prompt("Qual lanchinho você vai comer?")
+//console.log(`Aproveite o seu ${lanchinho}`)
+
